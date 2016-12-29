f23a94830d832b2d7f0088db71f33fa4d28d868d
koheisg <kohei.sg@gmail.com>
2016-12-29 18:36:33 +0900

    Add login and tweets
    - scaffold users and tweets
    - signup
    - login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    _current_user if session[:user_id]
+  end
+
+  def _current_user
+    @_current_user ||= User.find(session[:user_id])
+  end
 end
