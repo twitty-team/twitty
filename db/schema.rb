8905c2f512b621787f7a7a7512b6ac0a73420a53
koheisg <kohei.sg@gmail.com>
2017-06-05 00:05:00 +0900

    Change schema format migrate reset
    - rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,18 +13,18 @@
 ActiveRecord::Schema.define(version: 20161229093347) do
 
   create_table "tweets", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "body"
+    t.integer "user_id"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
