8572f4632473e3aaf02a8b25dbd08ea3c8ace0ec
koheisg <kohei.sg@gmail.com>
2017-06-13 01:06:21 +0900

    Make require login in /tweets/new

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :require_login, except: :index
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class TweetsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
     @tweet = tweets(:one)
   end
 
@@ -11,14 +12,14 @@ class TweetsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    login_as(@user.name, 'secret')
     get new_tweet_url
     assert_response :success
   end
 
   test "should create tweet" do
     assert_difference('Tweet.count') do
-      user = users(:one)
-      login_as(user.name, 'secret')
+      login_as(@user.name, 'secret')
       post tweets_url, params: { tweet: { body: @tweet.body } }
     end
 
@@ -26,21 +27,25 @@ class TweetsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show tweet" do
+    login_as(@user.name, 'secret')
     get tweet_url(@tweet)
     assert_response :success
   end
 
   test "should get edit" do
+    login_as(@user.name, 'secret')
     get edit_tweet_url(@tweet)
     assert_response :success
   end
 
   test "should update tweet" do
+    login_as(@user.name, 'secret')
     patch tweet_url(@tweet), params: { tweet: { body: @tweet.body } }
     assert_redirected_to tweet_url(@tweet)
   end
 
   test "should destroy tweet" do
+    login_as(@user.name, 'secret')
     assert_difference('Tweet.count', -1) do
       delete tweet_url(@tweet)
     end
