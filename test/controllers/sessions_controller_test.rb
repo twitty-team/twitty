dfcf62f27282439c0d24e02c461e20bf3313069b
Kohei Sugi <kohei.sg@gmail.com>
2017-08-06 18:25:17 +0900

    Add session controller test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,13 +6,29 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     @user.password = 'secret'
   end
 
-  test "should get login" do
+  test "should get login when user not login" do
     get login_url
     assert_response :success
   end
 
-  test "should create session" do
+  test "should redirect tweets_path when user login" do
+    login_as(@user.name, @user.password)
+    get login_url
+    assert_redirected_to tweets_url
+  end
+
+  test "should create session when user does not have session" do
     post login_url, params: { user: { name: @user.name, password: @user.password } }
-    assert_response :found
+    assert_redirected_to tweets_url
+  end
+
+  test "should redirect login page when bad password" do
+    post login_url, params: { user: { name: @user.name, password: 'wrong-password' } }
+    assert_redirected_to login_url
+  end
+
+  test "should destroy session" do
+    get logout_url
+    assert_redirected_to login_url
   end
 end
