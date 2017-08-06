5f1f4fe48bf94e472ee7f9166fccef58db735e8b
Kohei Sugi <kohei.sg@gmail.com>
2017-08-06 23:50:30 +0900

    Improve test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -23,6 +23,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to user_url(User.last)
   end
 
+  test "should not create user when same name" do
+    assert_no_difference('User.count') do
+      post users_url, params: { user: { name: @user.name, password: 'secret' } }
+    end
+  end
+
   test "should show user" do
     get user_url(@user)
     assert_response :success
@@ -36,6 +42,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should update user" do
     patch user_url(@user), params: { user: { name: @user.name, password: 'secret' } }
     assert_redirected_to user_url(@user)
+  end
+
+  test "should update user when exist name" do
+    User.create(name: 'hoge', password: 'secret')
+
+    patch user_url(@user), params: { user: { name: 'hoge', password: 'secret' } }
+    @user.reload
+    assert_not_equal @user.name, 'hoge'
   end
 
   test "should destroy user" do
